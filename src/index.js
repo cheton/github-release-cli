830de79b7e2ed980509c52202e136311fecadcde
Cheton Wu <cheton@gmail.com>
2019-01-18 00:58:05 +0800

    A few more small changes and bug fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 /* eslint no-console: 0 */
 /* eslint max-len: 0 */
-import path from 'path';
 import fs from 'fs';
-
-import program from 'commander';
-import minimatch from 'minimatch';
-import pkg from '../package.json';
-import * as LinkHeader from 'http-link-header';
-const URL = require('url-parse');
-import * as mime from 'mime-types';
-
+import path from 'path';
 import Octokit from '@octokit/rest';
+import program from 'commander';
+import * as LinkHeader from 'http-link-header';
+import * as mime from 'mime-types';
+import minimatch from 'minimatch';
+import parse from 'url-parse';
+import pkg from '../package.json';
+
 const octokit = new Octokit();
 
 program
@@ -22,7 +21,6 @@ program
     .option('-t, --tag <tag>', 'tag')
     .option('-n, --name <name>', 'name')
     .option('-b, --body <body>', 'body', false)
-    .option('-a, --anonymous', 'Use github API without token mainly for testing', false)
     .option('-d, --draft [value]', 'draft', function(val) {
         if (String(val).toLowerCase() === 'false') {
             return false;
@@ -40,7 +38,7 @@ program.parse(process.argv);
 
 const [command, ...args] = program.args;
 
-if (!program.anonymous) {
+if (!command !== 'list') {
     octokit.authenticate({
         type: 'oauth',
         token: program.token || process.env.GITHUB_TOKEN
@@ -48,13 +46,22 @@ if (!program.anonymous) {
 }
 
 function next(response) {
-  if (!response.headers || !response.headers.link) return false;
+    if (!response.headers || !response.headers.link) {
+        return false;
+    }
 
-  let link = LinkHeader.parse(response.headers.link).rel('next');
-  if (!link) return false;
-  link = URL(link[0].uri, null, true).query
-  if (!link) return false;
-  return parseInt(link.page);
+    let link = LinkHeader.parse(response.headers.link).rel('next');
+    if (!link) {
+        return false;
+    }
+
+    const url = parse(link[0].uri, null, true);
+    if (!url.query) {
+        return false;
+    }
+
+    const nextPage = parseInt(url.query.page);
+    return nextPage;
 }
 
 const fn = {
@@ -64,31 +71,33 @@ const fn = {
         let release;
 
         try {
-            console.log('> releases#getReleaseByTag');
-            release = await octokit.repos.getReleaseByTag({
+            console.log(`> getReleaseByTag: owner=${owner}, repo=${repo}, tag=${tag}`);
+            const res = await octokit.repos.getReleaseByTag({
                 owner: owner,
                 repo: repo,
-                tag: tag
+                tag: tag,
             });
+            release = res.data;
         } catch (err) {
             // Ignore
         }
 
         try {
             if (!release) {
-                console.log('> releases#createRelease');
-                result = await octokit.repos.createRelease({
+                console.log(`> createRelease: tag_name=${tag}, name=${name || tag}, draft=${!!draft}, prerelease=${!!prerelease}`);
+                const res = await octokit.repos.createRelease({
                     owner,
                     repo,
                     tag_name: tag,
                     name: name || tag,
                     body: body || '',
                     draft: !!draft,
-                    prerelease: !!prerelease
-                })
+                    prerelease: !!prerelease,
+                });
+                release = res.data;
             } else {
-                console.log('> releases#updateRelease');
-                release = await octokit.repos.updateRelease({
+                console.log(`> updateRelease: release_id=${release.id}, tag_name=${tag}, name=${name || tag}`);
+                const res = await octokit.repos.updateRelease({
                     owner,
                     repo,
                     release_id: release.id,
@@ -97,23 +106,23 @@ const fn = {
                     body: (body === undefined) ? release.body || '' : body || '',
                     draft: (draft === undefined) ? !!release.draft : false,
                     prerelease: (prerelease === undefined) ? !!release.prerelease : false,
-                })
+                });
+                release = res.data;
             }
 
             if (files.length > 0) {
-                console.log('> releases#uploadReleaseAsset');
+                console.log(`> uploadReleaseAsset: assets_url=${release.assets_url}`);
                 for (let i = 0; i < files.length; ++i) {
                     const file = files[i];
-                    console.log('#%d name="%s" filePath="%s"', i + 1, path.basename(file), file);
-
+                    console.log(`  #${i + 1}: name="${path.basename(file)}" filePath="${file}"`);
                     await octokit.repos.uploadReleaseAsset({
-                        url: release.data.upload_url,
+                        url: release.upload_url,
                         file: fs.createReadStream(file),
                         headers: {
-                            'content-type': mime.lookup('file') || 'application/octet-stream',
-                            'content-length': fs.statSync(file).size,
+                            'Content-Type': mime.lookup(file) || 'application/octet-stream',
+                            'Content-Length': fs.statSync(file).size,
                         },
-                        name: path.basename(asset),
+                        name: path.basename(file),
                     });
                 }
             }
@@ -127,39 +136,40 @@ const fn = {
         let release;
 
         try {
-            console.log('> releases#getReleaseByTag');
-            release = await octokit.repos.getReleaseByTag({
+            console.log(`> getReleaseByTag: owner=${owner}, repo=${repo}, tag=${tag}`);
+            const res = await octokit.repos.getReleaseByTag({
                 owner: owner,
                 repo: repo,
-                tag: tag
+                tag: tag,
             });
+            release = res.data;
         } catch (err) {
             console.error(err);
             return;
         }
 
         try {
-            console.log('> releases#listAssetsForRelease');
+            const release_id = release.id;
+            console.log(`> listAssetsForRelease: release_id=${release_id}`);
 
             let assets = [];
-            let _assets = null;
             let page = 1;
             do {
-                let _assets = await octokit.repos.listAssetsForRelease({owner, repo, release_id, per_page, page})
-                assets = assets.concat(_assets.data)
-                page = next(_assets)
+                const res = await octokit.repos.listAssetsForRelease({ owner, repo, release_id, page });
+                assets = assets.concat(res.data);
+                page = next(res);
             } while (page)
 
             const deleteAssets = assets.filter(asset => {
                 return patterns.some(pattern => minimatch(asset.name, pattern));
             });
-            console.log('assets=%d, deleteAssets=%d', assets.length, deleteAssets.length);
+            console.log(`  assets=${assets.length}, deleteAssets=${deleteAssets.length}`);
 
             if (deleteAssets.length > 0) {
-                console.log('> releases#deleteReleaseAsset');
+                console.log('> deleteReleaseAsset:');
                 for (let i = 0; i < deleteAssets.length; ++i) {
                     const asset = deleteAssets[i];
-                    console.log('#%d', i + 1, {
+                    console.log(`  #${i + 1}:`, {
                         id: asset.id,
                         name: asset.name,
                         label: asset.label,
@@ -167,9 +177,9 @@ const fn = {
                         size: asset.size,
                         download_count: asset.download_count,
                         created_at: asset.created_at,
-                        updated_at: asset.updated_at
+                        updated_at: asset.updated_at,
                     });
-                    await octokit.repos.deleteReleaseAsset({owner, repo, asset_id: asset.id })
+                    await octokit.repos.deleteReleaseAsset({ owner, repo, asset_id: asset.id });
                 }
             }
         } catch (err) {
@@ -177,7 +187,11 @@ const fn = {
         }
     },
     'list': async () => {
-        const releases = await octokit.repos.listReleases({ owner: program.owner, repo: program.repo, page: 1 });
+        const releases = await octokit.repos.listReleases({
+            owner: program.owner,
+            repo: program.repo,
+            page: 1,
+        });
         for (const release of releases.data) {
             console.log(`${release.name} (${release.tag_name})`);
         }
@@ -185,14 +199,14 @@ const fn = {
 }[command];
 
 async function main() {
-  try {
-    typeof fn === 'function' && await fn();
-  } catch (err) {
-    // message has token in the response
-    const message = err.message.replace(/https?:[^\s]*/g, (match) => match.replace(/\?.*/, ''))
-    console.log(message);
-    process.exit(1);
-  }
+    try {
+        typeof fn === 'function' && await fn();
+    } catch (err) {
+        // message has token in the response
+        const message = err.message.replace(/https?:[^\s]*/g, (match) => match.replace(/\?.*/, ''));
+        console.log(message);
+        process.exit(1);
+    }
 }
 
 main()
